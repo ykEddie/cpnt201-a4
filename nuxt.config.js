0147e1366a7ef12c2b167b7884092fe199516b14
Ed <edadeagbo@gmail.com>
2022-03-01 20:45:53 -0700

    added code to modules section of nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@storyblok/nuxt',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
